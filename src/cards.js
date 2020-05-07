cc4c5016427b4d465ae79255ca21de09c508ec83
Cristopher Barrios <cjrbarrios@gmail.com>
2020-05-07 09:58:22 -0600

    Lamemo

---
+++ src/cards.js
@@ -0,0 +1,9 @@
+const cards = [
+"dororo.jpg","dororo2.jpg","dororo3.jpg","dororo4.jpg",
+"dororo5.jpg","dororo6.jpg","dororo7.jpg","dororo8.jpg",
+"dororo9.jpg","dororo10.jpg","dororo11.jpg","dororo12.jpg",
+"dororo13.jpg","dororo14.jpg","dororo15.jpg","dororo16.jpg",
+"dororo17.jpg","dororo18.jpg"
+];
+
+export default cards;
